cc0787297840ff7bca7cd9ded1a5a5efec6ba5ca
Victor Campos <victorcampos@ufrj.br>
2014-03-06 10:46:05 +0000

    adding tamplete to database

--- root/middleware/db_connect.js
+++ root/middleware/db_connect.js
@@ -2,8 +2,8 @@ module.exports = function() {
   var mongoose = require('mongoose');
 
   var env_url = {
-    "test": "mongodb://localhost/test",
-    "development": "mongodb://localhost/development"
+    "test": "mongodb://localhost/{%= name %}-test",
+    "development": "mongodb://localhost/{%= name %}-development"
   };
 
   var url = env_url[process.env.NODE_ENV || "development"];
