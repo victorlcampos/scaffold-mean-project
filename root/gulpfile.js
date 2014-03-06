4d4d69ef0f074eb2dd627ab7242f2652b8122375
Victor Campos <victorcampos@ufrj.br>
2014-03-06 11:34:10 +0000

    change folder structure

--- root/gulpfile.js
+++ root/gulpfile.js
@@ -1,23 +1,23 @@
-var gulp       = require('gulp'),
-    nodemon    = require('gulp-nodemon'),
-    less       = require('gulp-less'),
-    jshint     = require('gulp-jshint'),
-    uglify     = require('gulp-uglify'),
-    concat     = require('gulp-concat'),
-    livereload = require('gulp-livereload'),
+var gulp         = require('gulp'),
+    nodemon      = require('gulp-nodemon'),
+    less         = require('gulp-less'),
+    jshint       = require('gulp-jshint'),
+    uglify       = require('gulp-uglify'),
+    concat       = require('gulp-concat'),
+    livereload   = require('gulp-livereload'),
     autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', ['styles', 'scripts', 'nodemon', 'watch']);
 
 gulp.task('styles', function() {
-  gulp.src('./server/assets/stylesheets/**/*.less')
+  gulp.src('./assets/stylesheets/**/*.less')
       .pipe(less({compress: true}))
       .pipe(autoprefixer())
       .pipe(gulp.dest('./public/stylesheets'));
 });
 
 gulp.task('scripts', function() {
-  gulp.src('./server/assets/javascripts/**/*.js')
+  gulp.src('./assets/javascripts/**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
     .pipe(concat('main.min.js'))
@@ -26,19 +26,17 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('nodemon', function () {
-  nodemon({script: 'app.js', watch: ['./server/controllers/**/*',
-                                     './server/models/**/*',
-                                     './server/routes/**/*',
+  nodemon({script: 'app.js', watch: ['./server/**/*',
                                      './middleware/**/*',
                                      './node_modules/**/*']});
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./server/assets/stylesheets/**/*.less', ['styles']);
-  gulp.watch('./server/assets/javascripts/**/*.js'  , ['scripts']);
+  gulp.watch('./assets/stylesheets/**/*.less', ['styles']);
+  gulp.watch('./assets/javascripts/**/*.js'  , ['scripts']);
 
   var livereloadServer = livereload();
-  gulp.watch(['server/views/**/*', 'public/**/*']).on('change', function(file) {
+  gulp.watch(['public/**/*']).on('change', function(file) {
     livereloadServer.changed(file.path);
   });
 });
